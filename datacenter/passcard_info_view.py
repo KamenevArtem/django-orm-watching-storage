67d1fd5a9a9e0d8ab95ea74c79b64b26c3952eb3
KamenevArtem <112734642+KamenevArtem@users.noreply.github.com>
2022-12-19 21:21:47 +0300

    Update passcard_info_view.py

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,14 +1,12 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
 from datacenter.storage_information_view import get_duration
 from datacenter.storage_information_view import format_duration
 from datacenter.storage_information_view import is_visit_long
 
 
 def passcard_info_view(request, passcode):
-    get_object_or_404(Passcard, pk=1)
     passcard = Passcard.objects.get(passcode=passcode)
     visits = Visit.objects.filter(passcard=passcard)
     passcarde_visits = []
