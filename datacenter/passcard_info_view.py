8d886bef04875c93fa47f7939dfb9a4bb231280d
KamenevArtem <112734642+KamenevArtem@users.noreply.github.com>
2022-12-19 06:32:37 +0300

    Less1 Django

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,21 +1,28 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from datacenter.storage_information_view import get_duration
+from datacenter.storage_information_view import format_duration
+from datacenter.storage_information_view import is_visit_long
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
-
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+    get_object_or_404(Passcard, pk=1)
+    passcard = Passcard.objects.get(passcode=passcode)
+    visits = Visit.objects.filter(passcard=passcard)
+    passcarde_visits = []
+    for visit in visits:
+        duration_of_visit = int(get_duration(visit))
+        is_visit_suspicious = is_visit_long(duration_of_visit)  
+        this_passcard_visits = {
+                'entered_at': visit.entered_at,
+                'duration': format_duration(duration_of_visit),
+                'is_strange': is_visit_suspicious
+            }
+        passcarde_visits.append(this_passcard_visits)
     context = {
         'passcard': passcard,
-        'this_passcard_visits': this_passcard_visits
+       'this_passcard_visits': passcarde_visits
     }
     return render(request, 'passcard_info.html', context)
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,53 @@
-from datacenter.models import Passcard
+import datetime
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils.timezone import localtime
+
+
+def get_duration(visit):
+    current_time = localtime()
+    entered_time = localtime(visit.entered_at)
+    leaved_time = localtime(visit.leaved_at)
+    if visit.leaved_at is None:
+        duration_of_visit = current_time - entered_time
+    else:
+        duration_of_visit = leaved_time - entered_time
+    duration_time = int(duration_of_visit.total_seconds())
+    return duration_time
+
+
+def format_duration(duration_time):
+    hours = duration_time // 3600
+    minutes = (duration_time % 3600) // 60
+    seconds = duration_time - (minutes * 60) - (hours * 3600)
+    time_in_storage = f"{hours}:{minutes}:{seconds}"
+    time_in_storage = datetime.datetime.strptime(time_in_storage, "%H:%M:%S")
+    return datetime.datetime.strftime(time_in_storage, "%H:%M:%S")
+
+
+def is_visit_long(duration):
+    suspicious_time = 3600
+    if duration > suspicious_time:
+        return "Посещение подозрительное"
+    else:
+        return "Подозрений нет"
 
 
 def storage_information_view(request):
-    # Программируем здесь
-
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
+    non_closed_visits = Visit.objects.filter(leaved_at = None)
+    for visit in non_closed_visits:
+        visit_time = get_duration(visit)
+        duration_of_visit = format_duration(visit_time)
+        is_strange = is_visit_long(visit_time)
+        non_closed_visit = [
+            {
+                'who_entered': visit.passcard,
+                'entered_at': visit.entered_at,
+                'duration': duration_of_visit,
+                'is_strange': is_strange,
+            }
+        ]
+        context = {
+            'non_closed_visits': non_closed_visit, 
         }
-    ]
-    context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
-    }
     return render(request, 'storage_information.html', context)
